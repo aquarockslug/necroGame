9dcf544479e8318a78a82173857da67f5bf5b1e1
aquarock <aqua.rock.slug@gmail.com>
2024-05-22 19:48:32 -0500

    plantTextures

--- public/game.js
+++ public/game.js
@@ -1,38 +1,66 @@
 class Necro extends Phaser.Scene {
+
+        plantTextures = [
+                'red', 'green', 'blue',
+                'red', 'green', 'blue',
+                'red', 'green', 'blue'
+        ]
+
         preload() {
-                this.load.image('flower', 'flower.png')
+                this.load.image('red', 'red.png')
+                this.load.image('green', 'green.png')
+                this.load.image('blue', 'blue.png')
         }
+
         create() {
                 const garden = new Garden({
                         scene: this,
                         x: config.width / 4,
                         y: config.height / 4,
-                        texture: 'flower'
+                        plantTextures: this.plantTextures
                 })
         }
 }
 
 class Garden extends Phaser.GameObjects.Sprite {
         boxes = []
-        spots = []
         plants = []
 
         constructor(config) {
                 super(config.scene, config.x, config.y, config.texture)
+                this.plantTextures = config.plantTextures
                 this.createBoxes()
-                this.spots.forEach((spot) => this.plants.push(
-                        this.scene.add.image(spot.x, spot.y, 'flower')))
-                this.plants.forEach((plant) => plant.setScale(0.1))
+                this.plants = this.createPlants()
+                console.log(this.plants)
+        }
+
+        createPlants() {
+                let newPlants = []
+                this.plants.forEach((plant) => newPlants.push(
+                        this.scene.add.image(plant.x, plant.y, plant.texture)
+                ))
+                newPlants.forEach((plant) => {
+                        plant.setScale(0.1)
+                        plant.setInteractive()
+                        plant.on('pointerdown', () => {
+                                // syringe.fill(plant.texture)
+                                console.log(plant.texture.key)
+                        })
+                })
+                return newPlants
         }
 
         createBoxes(width = 3, height = 3) {
+                let i = 0
                 let pushRect = (x, y) => {
                         const boxPos = [this.x + x * 100, this.y + y * 100, 100, 100]
                         this.boxes[x][y] = new Phaser.Geom.Rectangle(...boxPos);
-                        this.spots.push({
+                        this.plants.push({
                                 x: this.boxes[x][y].centerX,
                                 y: this.boxes[x][y].centerY,
+                                texture: this.plantTextures[i]
                         })
+                        i++
                 }
                 for (let x = 0; x < width; x++) {
                         this.boxes[x] = [];
